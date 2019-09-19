4c607262c636789801c1aa7b65ccdd6743dc4a33
bima rijaluddin <bimarijal@gmail.com>
2019-09-19 11:28:06 +0800

    Tugas 2 Javascript
    Tugas dari niomic

---
+++ tugas2.js
@@ -0,0 +1,26 @@
+// tipe data number
+var number = 100;
+
+//tipe data string
+var string = "tugas-2-javascript";
+
+//tipe data boolean
+var boolean = 10;
+
+if(boolean <= 10)
+boolean = true;
+else
+boolean = false;
+
+//tipe data undefined
+var empty;
+
+//tipe data array
+var array = [1,"kata",2,3];
+
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(empty);
+console.log(array[1]);
